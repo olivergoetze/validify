2240778035fdf30b1c57620d724792ba927f2afa
Oliver Götze <oliver.goetze@mailbox.org>
2020-01-07 15:08:08 +0100

    Refactor messages. Add requirements.txt.

--- validify/helpers/messages.py
+++ validify/helpers/messages.py
@@ -1,7 +1,27 @@
 def get_message_by_id(message_id: str, lang: str="de") -> str:
     if lang == "de":
         if message_id == "0001":
-            message_str = ""
+            message_str = "Element {} enthält keine Subelemente, obwohl eines oder mehrere Subelemente erwartet."
+        elif message_id == "0002":
+            message_str = "Element {} enthält keinen Elementinhalt, obwohl das Element nicht leer sein darf."
+        elif message_id == "0003":
+            message_str = "Element {} enthält ein nicht erwartetes Attribut: {}"
+        elif message_id == "0004":
+            message_str = "Element {} enthält nicht das Pflicht-Attribut {}."
+        elif message_id == "0005":
+            message_str = "Element {} enthält ein nicht erwartetes Subelement: {}."
+        elif message_id == "0006":
+            message_str = "Element {} enthält nicht das Pflicht-Subelement {}."
+        elif message_id == "0007":
+            message_str = "Element {} ist {} mal vorhanden, erwartet wird jedoch nur {} Vorkommen."
+        elif message_id == "0008":
+            message_str = "Element {} should not contain text character content."
+        elif message_id == "0009":
+            message_str = "Element {} should not contain tail character content."
+        elif message_id == "0010":
+            message_str = "{} -- Element value not eqal to required value: '{}' != '{}'"
+        elif message_id == "0011":
+            message_str = "{}, attribute {} -- attribute value not eqal to required value: '{}' != '{}'"
         else:
             message_str = ""
     else:
--- validify/validify.py
+++ validify/validify.py
@@ -3,7 +3,7 @@
 from lxml import etree
 from logzero import logger
 import collections
-import re
+# import re
 
 from validify.helpers.cleanup_compare_strings import get_compare_value
 from validify.helpers import normalize_space
@@ -11,7 +11,7 @@ from validify.helpers import messages
 from validify.helpers import examples
 
 
-def assess_element_structure(element: etree.Element, element_sourceline: int, xmlns_def: dict, validation_rules: dict, validation_messages: list, validation_results: list) -> list:
+def assess_element_structure(element: etree.Element, element_sourceline: int, xmlns_def: dict, validation_rules: dict, validation_messages: list, validation_results: list, message_lang: str) -> list:
     element_name = element.tag
     element_attributes = element.attrib
     element_subelements = [subelement.tag for subelement in element]
@@ -25,90 +25,101 @@ def assess_element_structure(element: etree.Element, element_sourceline: int, xm
             # element children optional
             if not validation_rules_set["element_children_optional"]:
                 if len(element) == 0:
-                    message_text = "Element {} enthält keine Subelemente, obwohl eines oder mehrere Subelemente erwartet.".format(element_name)
+                    message_id = "0001"
+                    message_text = messages.get_message_by_id(message_id, message_lang).format(element_name)
                     validation_messages.append(message_text)
-                    validation_results.append({"message_id": "0001", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                    validation_results.append({"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # element content optional
             if not validation_rules_set["element_content_optional"]:
                 if get_compare_value(element) == "":
-                    message_text = "Element {} enthält keinen Elementinhalt, obwohl das Element nicht leer sein darf.".format(element_name)
+                    message_id = "0002"
+                    message_text = messages.get_message_by_id(message_id, message_lang).format(element_name)
                     validation_messages.append(message_text)
                     validation_results.append(
-                        {"message_id": "0002", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                        {"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # optional attributes
             for element_attribute in element_attributes:
                 if (element_attribute not in validation_rules_set["optional_attributes"]) and (element_attribute not in validation_rules_set["obligatory_attributes"]):
-                    message_text = "Element {} enthält ein nicht erwartetes Attribut: {}".format(element_name, element_attribute)
+                    message_id = "0003"
+                    message_text = messages.get_message_by_id(message_id, message_lang).format(element_name, element_attribute)
                     validation_messages.append(message_text)
                     validation_results.append(
-                        {"message_id": "0003", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                        {"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # obligatory attributes
             for obligatory_attribute in validation_rules_set["obligatory_attributes"]:
                 if obligatory_attribute not in element_attributes:
-                    message_text = "Element {} enthält nicht das Pflicht-Attribut {}.".format(element_name, obligatory_attribute)
+                    message_id = "0004"
+                    message_text = messages.get_message_by_id(message_id, message_lang).format(element_name, obligatory_attribute)
                     validation_messages.append(message_text)
                     validation_results.append(
-                        {"message_id": "0004", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                        {"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # optional subelements
             for element_subelement in element_subelements:
                 if (element_subelement not in validation_rules_set["optional_subelements"]) and (element_subelement not in validation_rules_set["obligatory_subelements"]):
-                    message_text = "Element {} enthält ein nicht erwartetes Subelement: {}.".format(element_name, element_subelement)
+                    message_id = "0005"
+                    message_text = messages.get_message_by_id(message_id, message_lang).format(element_name, element_subelement)
                     validation_messages.append(message_text)
                     validation_results.append(
-                        {"message_id": "0005", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                        {"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # obligatory subelements
             for obligatory_subelement in validation_rules_set["obligatory_subelements"]:
                 if obligatory_subelement not in element_subelements:
-                    message_text = "Element {} enthält nicht das Pflicht-Subelement {}.".format(element_name, obligatory_subelement)
+                    message_id = "0006"
+                    message_text = messages.get_message_by_id(message_id, message_lang).format(element_name, obligatory_subelement)
                     validation_messages.append(message_text)
                     validation_results.append(
-                        {"message_id": "0006", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                        {"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # max occurence
             if validation_rules_set["max_occurence"] is not None:  # max occurence nur prüfen, wenn das Element nicht unbegrenzt auftreten kann
                 element_siblings = element.getparent().findall(element_name, namespaces=xmlns_def)
                 if len(element_siblings) > validation_rules_set["max_occurence"]:
-                    message_text = "Element {} ist {} mal vorhanden, erwartet wird jedoch nur {} Vorkommen.".format(element_name, len(element_siblings), validation_rules_set["max_occurence"])
+                    message_id = "0007"
+                    message_text = messages.get_message_by_id(message_id, message_lang).format(element_name, len(element_siblings), validation_rules_set["max_occurence"])
                     validation_messages.append(message_text)
                     validation_results.append(
-                        {"message_id": "0007", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                        {"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # character content allowed
             if validation_rules_set["text_character_content_allowed"] is False:
                 if element.text is not None:
                     if element.text != "":
-                        message_text = "Element {} should not contain text character content.".format(element_name)
+                        message_id = "0008"
+                        message_text = messages.get_message_by_id(message_id, message_lang).format(element_name)
                         validation_messages.append(message_text)
                         validation_results.append(
-                            {"message_id": "0008", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                            {"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
             if validation_rules_set["tail_character_content_allowed"] is False:
                 if element.tail is not None:
                     if element.tail != "":
-                        message_text = "Element {} should not contain tail character content.".format(element_name)
+                        message_id = "0009"
+                        message_text = messages.get_message_by_id(message_id, message_lang).format(element_name)
                         validation_messages.append(message_text)
                         validation_results.append(
-                            {"message_id": "0009", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                            {"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # allowed values (xs:enumeration)
             if len(validation_rules_set["allowed_values"]) > 0:
                 if element.text is not None:
                     if element.text not in validation_rules_set["allowed_values"]:
-                        message_text = "{} -- Element value not eqal to required value: '{}' != '{}'".format(element_name, element.text, ", ".join(validation_rules_set["allowed_values"]))
+                        message_id = "0010"
+                        message_text = messages.get_message_by_id(message_id, message_lang).format(element_name, element.text, ", ".join(validation_rules_set["allowed_values"]))
                         validation_messages.append(message_text)
-                        validation_results.append({"message_id": "0010", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                        validation_results.append({"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
             # Attribute definition
             for attribute_definition in validation_rules_set["attribute_def"]:
                 if attribute_definition["attribute_name"] in element.attrib:
                     if element.attrib[attribute_definition["attribute_name"]] not in attribute_definition["allowed_values"]:
-                        message_text = "{}, attribute {} -- attribute value not eqal to required value: '{}' != '{}'".format(element_name, attribute_definition["attribute_name"], element.attrib[attribute_definition["attribute_name"]], ", ".join(attribute_definition["allowed_values"]))
+                        message_id = "0011"
+                        message_text = messages.get_message_by_id(message_id, message_lang).format(element_name, attribute_definition["attribute_name"], element.attrib[attribute_definition["attribute_name"]], ", ".join(attribute_definition["allowed_values"]))
                         validation_messages.append(message_text)
-                        validation_results.append({"message_id": "0011", "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
+                        validation_results.append({"message_id": message_id, "message_text": message_text, "element_name": element_name, "element_sourceline": element_sourceline})
 
 
 
@@ -117,12 +128,20 @@ def assess_element_structure(element: etree.Element, element_sourceline: int, xm
 
 
 
-def validate(input_file: str, xmlns_def=None, validation_rules=None):
+def validate(input_file: str, xmlns_def=None, validation_rules=None, message_lang=None):
     if xmlns_def is None:
         xmlns_def = {}
     if validation_rules is None:
         validation_rules = examples.compile_example_rules()
         logger.warn("No validation rules defined; using example rules for validation.")
+    message_lang_options = ["de", "en"]
+    if message_lang is None:
+        message_lang = "de"
+        logger.info("No message language option delivered; using standard value 'de'.")
+    elif message_lang not in message_lang_options:
+        message_lang = "de"
+        logger.warn("No valid message language option delivered; using standard value 'de'.")
+
     validation_messages = []
     validation_results = []
 
@@ -132,7 +151,7 @@ def validate(input_file: str, xmlns_def=None, validation_rules=None):
         for xml_element in xml_elements:
             xml_element_sourceline = xml_element.sourceline  # get original source line before applying normalize-space
             normalize_space.parse_xml_content(xml_element)  # apply normalize-space so only actual character content is found
-            validation_results = assess_element_structure(xml_element, xml_element_sourceline, xmlns_def, validation_rules, validation_messages, validation_results)
+            validation_results = assess_element_structure(xml_element, xml_element_sourceline, xmlns_def, validation_rules, validation_messages, validation_results, message_lang)
     except etree.XMLSyntaxError:
         logger.error("Input file {} is not a well-formed XML document.".format(input_file))
 
