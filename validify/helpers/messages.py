8fe871f93454dc8da52f6f6abba57e73bc4e8ce8
Oliver Götze <oliver.goetze@mailbox.org>
2020-01-08 08:43:08 +0100

    Further cleanup of validation message strings.

--- validify/helpers/messages.py
+++ validify/helpers/messages.py
@@ -5,7 +5,7 @@ def get_message_by_id(message_id: str, lang: str="de") -> str:
         elif message_id == "0002":
             message_str = "Element {} enthält keinen Elementinhalt, obwohl das Element nicht leer sein darf."
         elif message_id == "0003":
-            message_str = "Element {} enthält ein nicht erwartetes Attribut: {}"
+            message_str = "Element {} enthält ein nicht erwartetes Attribut: {}."
         elif message_id == "0004":
             message_str = "Element {} enthält nicht das Pflicht-Attribut {}."
         elif message_id == "0005":
@@ -15,6 +15,31 @@ def get_message_by_id(message_id: str, lang: str="de") -> str:
         elif message_id == "0007":
             message_str = "Element {} ist {} mal vorhanden, erwartet wird jedoch nur {} Vorkommen."
         elif message_id == "0008":
+            message_str = "Element {} darf keinen Text (Character Content) im Elementinhalt enthalten."
+        elif message_id == "0009":
+            message_str = "Element {} darf keinen Text (Character Content) vor dem Folgeelement (Tail) enthalten."
+        elif message_id == "0010":
+            message_str = "{} -- Tatsächlicher Elementwert entspricht nicht den zulässigen Werten: '{}' != '{}'."
+        elif message_id == "0011":
+            message_str = "{}, Attribut {} -- Tatsächlicher Attributwert entspricht nicht den zulässigen Werten: '{}' != '{}'."
+        else:
+            message_str = ""
+    else:
+        if message_id == "0001":
+            message_str = "Element {} does not contain any subelements, although one or more subelements is expected."
+        elif message_id == "0002":
+            message_str = "Element {} does not contain any text, although it is not expected to be empty."
+        elif message_id == "0003":
+            message_str = "Element {} contains an unexpected attribute: {}."
+        elif message_id == "0004":
+            message_str = "Element {} misses the mandatory attribute {}."
+        elif message_id == "0005":
+            message_str = "Element {} contains an unexpected subelement: {}."
+        elif message_id == "0006":
+            message_str = "Element {} misses the mandatory subelement {}."
+        elif message_id == "0007":
+            message_str = "Element {} occurs {} times, though it is expected to occur only {} times."
+        elif message_id == "0008":
             message_str = "Element {} should not contain text character content."
         elif message_id == "0009":
             message_str = "Element {} should not contain tail character content."
@@ -22,11 +47,6 @@ def get_message_by_id(message_id: str, lang: str="de") -> str:
             message_str = "{} -- Element value not eqal to required value: '{}' != '{}'"
         elif message_id == "0011":
             message_str = "{}, attribute {} -- attribute value not eqal to required value: '{}' != '{}'"
-        else:
-            message_str = ""
-    else:
-        if message_id == "0001":
-            message_str = ""
         else:
             message_str = ""
 
