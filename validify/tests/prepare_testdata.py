2710d2f794f0c55307319e720ff7407e68b82d29
Oliver Götze <oliver.goetze@mailbox.org>
2020-01-14 16:54:53 +0100

    Prepare tests.

---
+++ validify/tests/prepare_testdata.py
@@ -0,0 +1,35 @@
+def compile_test_rules() -> dict:
+    validation_rules = {"element": []}
+    ruleset = {}
+
+    ruleset["element_content_optional"] = False
+    ruleset["element_children_optional"] = False
+    ruleset["optional_attributes"] = ["valid_optional_attribute_01", "valid_optional_attribute_02"]
+    ruleset["obligatory_attributes"] = ["obligatory_attribute_01", "obligatory_attribute_02"]
+    ruleset["optional_subelements"] = ["optional_subelement_01", "optional_subelement_02"]
+    ruleset["obligatory_subelements"] = ["obligatory_subelement_01", "obligatory_subelement_02"]
+    ruleset["max_occurence"] = 2
+    ruleset["text_character_content_allowed"] = True
+    ruleset["tail_character_content_allowed"] = False
+
+    ruleset["allowed_values"] = ["valid_value_01", "valid_value_02"]
+    ruleset["allowed_patterns"] = ["^test-\d{4}$", "^test-\d{3}$"]
+    ruleset["allowed_datatypes"] = []
+    ruleset["attribute_def"] = []
+    ruleset["attribute_def"].append({"attribute_name": "valid_optional_attribute_01", "allowed_values": ["valid_value_01", "valid_value_02"],
+                                     "allowed_patterns": ["^test-\d{4}$", "^test-\d{3}$"]})
+    ruleset["attribute_def"].append(
+        {"attribute_name": "obligatory_attribute_01", "allowed_values": ["valid_value_01", "valid_value_02"],
+         "allowed_patterns": []})
+
+
+    ruleset["rule_conditions"] = []
+    ruleset["rule_conditions"].append(
+        {"text_values": ["valid_text_value"],
+         "attribute_def": [{"attribute_name": "valid_attribute_name", "allowed_values": ["valid_value"]},
+                           {"attribute_name": "another_valid_attribute_name", "allowed_values": ["valid_value"]}],
+         "reference_elements": [{"element_name": "reference_element", "attribute_def": [{"attribute_name": "reference_test", "allowed_values": ["valid_value"]}],"preceding_elements": 1}]})
+
+    validation_rules["element"].append(ruleset)
+
+    return validation_rules
--- validify/tests/test_main.py
+++ validify/tests/test_main.py
@@ -1,2 +1,59 @@
-def test_example():
-    assert 1 == 2
+import validify
+from validify.tests.prepare_testdata import compile_test_rules
+
+class TestElementStructureAsssessment:
+
+    def test_rule_condition_text_values(self):
+        validation_result = validify.validate("validify/tests/test_rule_condition_text_values.xml", validation_rules=compile_test_rules(), log_to_console=False)
+        assert len(validation_result) == 0
+
+    def test_rule_condition_attribute_values(self):
+        validation_result = validify.validate("validify/tests/test_rule_condition_attribute_values.xml",
+                                              validation_rules=compile_test_rules(), log_to_console=False)
+        assert len(validation_result) == 0
+
+    def test_rule_condition_reference_elements(self):
+        validation_result = validify.validate("validify/tests/test_rule_condition_reference_elements.xml", validation_rules=compile_test_rules(), log_to_console=False)
+        assert len(validation_result) == 0
+
+
+    def test_rule_element_children_optional(self):
+        pass
+
+    def test_rule_element_content_optional(self):
+        pass
+
+
+    def test_rule_optional_attributes(self):
+        pass
+
+    def test_rule_obligatory_attributes(self):
+        pass
+
+
+    def test_rule_optional_subelements(self):
+        pass
+
+    def test_rule_obligatory_subelements(self):
+        pass
+
+
+    def test_rule_max_occurence(self):
+        pass
+
+    def test_rule_character_content_allowed(self):
+        pass
+
+    def test_rule_allowed_values(self):
+        pass
+
+    def test_rule_allowed_patterns(self):
+        pass
+
+
+
+    def test_attribute_definition_allowed_values(self):
+        pass
+
+    def test_attribute_definition_allowed_patterns(self):
+        pass
